eb47f0acad449355cfe29559101422097276aeb2
Fernando Rodrigues <fernando_tecti@hotmail.com>
2023-07-20 23:39:23 -0300

    making the template

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TodoApp</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins&family=Rubik:wght@400;500&family=Ubuntu:wght@500&display=swap">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="sidebar">
+            Logo
+        </div>
+        <div class="content">
+            <nav>
+                <a href="#" class="btn btn-primary">
+                    Criar tarefa
+                </a>
+            </nav>
+            <main>
+                <section class="graph">
+                    Gráfico
+                </section>
+                <section class="list">
+                    Lista
+                </section>
+            </main>
+        </div>
+    </div>
+</body>
+
+</html>
