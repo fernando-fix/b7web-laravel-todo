b3cb022c65c0867ff1ec108e29d4424900c93478
Fernando Rodrigues <fernando_tecti@hotmail.com>
2023-07-25 21:42:18 -0300

    layout finished

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -8,14 +8,15 @@
     <title>TodoApp</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins&family=Rubik:wght@400;500&family=Ubuntu:wght@500&display=swap">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Poppins&family=Rubik:wght@400;500&family=Ubuntu:wght@500&display=swap">
     <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 
 <body>
     <div class="container">
         <div class="sidebar">
-            Logo
+            <img class="sidebar-logo" src="assets/img/laravel-logo.svg" alt="logo">
         </div>
         <div class="content">
             <nav>
@@ -25,10 +26,72 @@
             </nav>
             <main>
                 <section class="graph">
-                    Gráfico
+                    <div class="graph-header">
+                        <h2> Progresso do dia</h2>
+                        <div class="graph-hr">
+                            <hr>
+                        </div>
+                        <div class="graph-data">
+                            <img src="/assets/img/icon-prev.png" alt="">
+                            <span>13 de Dez</span>
+                            <img src="/assets/img/icon-next.png" alt="">
+                        </div>
+                    </div>
+                    <div class="graph-header-subtitle">
+                        <span>Tarefas: <strong>3/6</strong></span>
+                    </div>
+                    <div class="graph-placeholder">
+                        {{-- Área do gráfico --}}
+                    </div>
+                    <div class="graph-header-tasks-left">
+                        <img src="/assets/img/icon-info.png" alt="alerta de tarefas">
+                        Restam 3 tarefas para serem realizadas
+                    </div>
                 </section>
                 <section class="list">
-                    Lista
+                    <div class="list-header">
+                        <select class="list-header-select">
+                            <option value="1">Todas as tarefas</option>
+                        </select>
+                    </div>
+                    <div class="task-list">
+                        <div class="task">
+                            <div class="title">
+                                <input type="checkbox" name="" id="">
+                                <div class="task-title">Título da tarefa</div>
+                            </div>
+                            <div class="priority">
+                                <div class="sphere"></div>
+                                <div>Título da tarefa</div>
+                            </div>
+                            <div class="actions">
+                                <a href="#">
+                                    <img src="/assets/img/icon-edit.png" alt="edit-task">
+                                </a>
+                                <a href="#">
+                                    <img src="/assets/img/icon-delete.png" alt="delete task">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="task">
+                            <div class="title">
+                                <input type="checkbox" name="" id="">
+                                <div class="task-title">Título da tarefa</div>
+                            </div>
+                            <div class="priority">
+                                <div class="sphere"></div>
+                                <div>Título da tarefa</div>
+                            </div>
+                            <div class="actions">
+                                <a href="#">
+                                    <img src="/assets/img/icon-edit.png" alt="edit-task">
+                                </a>
+                                <a href="#">
+                                    <img src="/assets/img/icon-delete.png" alt="delete task">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </main>
         </div>
